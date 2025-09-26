a83597474ee4137a1af6040f8b35b626b3a48eac
ST10355439 <ST10355439@vcconnect.edu.za>
2025-09-26 15:40:39 +0200

    Third Commit: Added Authentication with JWT

--- pulsevote-backend/app.js
+++ pulsevote-backend/app.js
@@ -1,15 +1,21 @@
 const express = require('express');
-const cors = require('cors'); // this will be discussed later
+const cors = require('cors'); 
 const helmet = require('helmet'); // this will be discussed later
 const dotenv = require('dotenv');
+const authRoutes = require("./routes/authRoutes");
+const { protect } = require("./middleware/authMiddleware");
 
 dotenv.config();
 
 const app = express();
 
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: "https://localhost:5173",
+  credentials: true
+}));
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.get('/', (req, res) => {
 res.send('PulseVote API running!');
@@ -17,6 +23,13 @@ res.send('PulseVote API running!');
 
 app.get('/test', (req, res) => {
     res.send('Testing PulseVote API!');
+});
+
+app.get("/api/protected", protect, (req, res) => {
+  res.json({
+    message: `Welcome, user ${req.user.id}! You have accessed protected data.`,
+    timestamp: new Date()
+  });
 });
 
 module.exports = app;
