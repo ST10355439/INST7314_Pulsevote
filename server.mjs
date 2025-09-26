b7df7669b3b54fa5abf2a618954dd1db170d57dd
ST10355439 <ST10355439@vcconnect.edu.za>
2025-09-26 14:21:29 +0200

    First Commit: Completed Lab Guide instructions up until page 33

---
+++ server.mjs
@@ -0,0 +1,34 @@
+import https from "https";
+import http from "http";
+import fs from "fs";
+import posts from "./routes/post.mjs";
+//import users from "./routes/user.mjs";
+import express from "express"
+//import cors from "cors"
+
+const PORT = 3001;
+const app = express();
+
+const options = {
+    key: fs.readFileSync('keys/privatekey.pem'),
+    cert: fs.readFileSync('keys/certificate.pem')
+}
+
+//app.use(cors());
+app.use(express.json());
+
+app.use((reg,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    next();
+})
+
+app.use("/post", posts);
+app.route("/post", posts);
+//app.use("/user", users);
+//app.route("/user", users);
+
+let server = https.createServer(options, app)
+
+server.listen(PORT);
